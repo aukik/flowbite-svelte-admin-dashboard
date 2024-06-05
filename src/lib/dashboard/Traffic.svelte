99db59facf7ae251cac56f24af62c0c28738d7b2
aukik <aukikaurnabx@gmail.com>
2024-06-05 21:41:31 +0600

    feat: Update chart labels in dashboard and traffic graphs

--- src/lib/dashboard/Traffic.svelte
+++ src/lib/dashboard/Traffic.svelte
@@ -2,7 +2,7 @@
 	import { Card, Chart } from 'flowbite-svelte';
 	import options from '$lib/graphs/traffic';
 	import SmallPanel from './SmallPanel.svelte';
-	import { DesktopPcOutline, MobilePhoneOutline, TabletOutline } from 'flowbite-svelte-icons';
+	import { UsersGroupSolid, BookOpenSolid, BriefcaseSolid } from 'flowbite-svelte-icons';
 	import More from '$lib/widgets/More.svelte';
 
 	export let dark: boolean = false;
@@ -13,23 +13,24 @@
 		class="mb-4 items-center justify-between border-b border-gray-200 pb-4 dark:border-gray-700 sm:flex"
 	>
 		<div class="mb-4 w-full sm:mb-0">
-			<h3 class="text-base font-normal text-gray-500 dark:text-gray-400">Traffic by device</h3>
+			<!-- <h3 class="text-base font-normal text-gray-500 dark:text-gray-400">Traffic by device</h3> -->
 			<span class="text-2xl font-bold leading-none text-gray-900 dark:text-white sm:text-3xl"
-				>Desktop</span
+				>Total Users</span
 			>
+			<h3 class="text-base font-normal text-gray-500 dark:text-gray-400">percentage %</h3>
 		</div>
-		<More title="Full report" href="#top" />
+		<!-- <More title="Full report" href="#top" /> -->
 	</div>
 	<Chart options={options(dark)}></Chart>
 	<div class="mb-4 flex items-center justify-between pt-4 sm:pt-6 lg:justify-evenly">
-		<SmallPanel title="Desktop" subtitle="234k" change={4}>
-			<DesktopPcOutline slot="icon" size="xl" class="mb-1" />
+		<SmallPanel title="Student" subtitle="22" change={4}>
+			<UsersGroupSolid slot="icon" size="xl" class="mb-1" />
 		</SmallPanel>
-		<SmallPanel title="Phone" subtitle="94k" change={-1}>
-			<MobilePhoneOutline slot="icon" size="xl" class="mb-1" />
+		<SmallPanel title="Sponsors" subtitle="4" change={-1}>
+			<BookOpenSolid slot="icon" size="xl" class="mb-1" />
 		</SmallPanel>
-		<SmallPanel title="Tablet" subtitle="16k" change={-0.6}>
-			<TabletOutline slot="icon" size="xl" class="mb-1" />
+		<SmallPanel title="Teacher" subtitle="12" change={-0.6}>
+			<BriefcaseSolid slot="icon" size="xl" class="mb-1" />
 		</SmallPanel>
 	</div>
 </Card>
